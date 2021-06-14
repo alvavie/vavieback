d04214393e96bee32c8cba1bca307a97237f3911
Alvaro Artieda <alvaro.artieda@gmail.com>
2021-06-14 18:23:50 +0200

    add routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const router = express.Router();
 
 const app = express();
 
@@ -100,3 +101,5 @@ function initial() {
         }
     });
 }
+
+module.exports = router ;
